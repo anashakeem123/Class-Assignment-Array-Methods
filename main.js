3e0270c7298c10d8eac481090e6d35d40dcaa866
Anas Hakeem <160387360+anashakeem123@users.noreply.github.com>
2024-05-11 14:46:27 +0500

    Push My Class Assignment Array Methods

---
+++ main.js
@@ -0,0 +1,3 @@
+// Array items Of collection
+var Names = ["Anas", "Talha", "Rehman", "SSSSS"];
+console.log(Names.slice(1, 2));
---
+++ main.ts
@@ -0,0 +1,14 @@
+// Array items Of collection data
+// let Names = ["Anas","Talha","Rehman","SSSSS"]
+// console.log(Names.slice(1,2)) // Shell Or Copy Create 
+
+
+// Array Methods Class Assignment : (POP)(PUSH)(SHIFT)(UNSHIFT)(SLICE)(SPLICE)
+let arr = ["Anas","Talha","Rehman","Hakeem"]
+console.log(arr.pop()) // Array POP method last Index Remove
+console.log(arr.push("Ali")) // Array PUSH method last Index Add
+console.log(arr.shift()) // Array shift method First Index Remove
+console.log(arr.unshift("Khan")) // Array unshift method First Index Add
+console.log(arr.slice(1,3)) // Array slice method  Two Parameters 
+console.log(arr.splice(1,3,"Haider","Bhai")) // Array splice method  Three Parameters 
+
